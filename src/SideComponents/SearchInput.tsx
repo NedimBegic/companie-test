70c3d99396a9940a4800134e0aebf6f92ad12a37
nedim begic <nedim.begic93@gmail.com>
2024-05-03 21:19:55 +0200

    writing functionality for searchBar

--- src/SideComponents/SearchInput.tsx
+++ src/SideComponents/SearchInput.tsx
@@ -1,7 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
+import { useAppContext } from "../store/appContext";
 
 const SearchInput: React.FC<{ style: string }> = ({ style }) => {
-  return <input type="text" placeholder="Search..." className={style} />;
+  const {
+    searchBarHandler,
+    searchBar,
+    findBySearchHandler,
+    activeButton,
+    searchMoviesHandler,
+    searchTvShowsHandler,
+  } = useAppContext();
+  const [timer, setTimer] = useState<NodeJS.Timeout | null>(null);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+    searchBarHandler(value);
+
+    if (timer) {
+      clearTimeout(timer);
+    }
+
+    // Set up calling functionality depending on value in search bar
+    const newTimer = setTimeout(() => {
+      if (value.length >= 3) {
+        findBySearchHandler();
+      } else {
+        if (activeButton === "movies") {
+          searchMoviesHandler();
+        } else if (activeButton === "tvShows") {
+          searchTvShowsHandler();
+        }
+      }
+    }, 1000);
+
+    setTimer(newTimer);
+  };
+
+  return (
+    <input
+      type="text"
+      placeholder="Search..."
+      className={style}
+      value={searchBar}
+      onChange={handleChange}
+    />
+  );
 };
 
 export default SearchInput;
